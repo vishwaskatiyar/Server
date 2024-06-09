c62781c162b53866a90fd795b720edf73629f4a2
Vishwas katiyar <vishwaskatiyar39@gmail.com>
2024-06-09 15:21:25 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// Import the JSON data
+const data = require('../data.json');
+
+// Define a route to serve the JSON data
+app.get('/', (req, res) => {
+    res.json(data);
+});
+app.get('/api/data', (req, res) => {
+    res.json(data);
+});
+
+// Export the app to use as a serverless function
+module.exports = app;
